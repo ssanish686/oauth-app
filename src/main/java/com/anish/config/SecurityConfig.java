0bfefe2ef9279598767359b3f772c20ecc56cc3a
ssanish686 <58726205+ssanish686@users.noreply.github.com>
2024-02-09 16:08:35 +0530

    Actuator changes

--- src/main/java/com/anish/config/SecurityConfig.java
+++ src/main/java/com/anish/config/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().sessionManagement()
+		http.csrf().disable().authorizeRequests().antMatchers("/actuator","/actuator/**").permitAll()
+				.anyRequest().authenticated().and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.NEVER);
 	}
 
